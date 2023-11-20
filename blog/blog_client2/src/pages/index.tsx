70f449261910f0c1bf66cb8bab93cc7a404fa0bd
gadname <gakugaku1114@icloud.com>
2023-11-20 16:49:16 +0900

    TopタイトルのCSS修正

--- blog/blog_client2/src/pages/index.tsx
+++ blog/blog_client2/src/pages/index.tsx
@@ -9,9 +9,9 @@ export default function Top() {
       <Header />
       <Main />
       <Fade>
-      <h1 className="top_text">AIで、想像を</h1>
+      <h1 className="top_text">AIで、<br></br>想像を超えた<br></br>芸術を。</h1>
       </Fade>
-      <Link href="/home">
+      <Link href="/home" className="gotoHome">
         <div>Go to Home</div>
       </Link>
      
